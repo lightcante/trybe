d769d05f977d3bfea81be9eacae5cbea533320e1
Justin Webb <jw@justinwebb.io>
2015-05-04 11:51:11 -0700

    Establish initial Gruntfile layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,44 @@
-/* 
+/*
 * @Author: justinwebb
 * @Date:   2015-05-04 11:30:21
 * @Last Modified by:   justinwebb
-* @Last Modified time: 2015-05-04 11:30:21
+* @Last Modified time: 2015-05-04 11:51:03
 */
 
 'use strict';
+module.exports = function (grunt) {
+  var taskConfig;
+  var userConfig;
+  var _ = grunt.util._;
+
+  // ----------------------------------------------------
+  // Task configuration
+  // ----------------------------------------------------
+  // serverConfig = require('./server/server-config.js');
+  userConfig = require('./build-config.js');
+  taskConfig = {
+
+    pkg: grunt.file.readJSON('package.json')
+
+  };
+  // Combine into external build configration file and task
+  // configuration into Grunt's initConfig
+  grunt.initConfig(_.extend(taskConfig, userConfig));
+
+
+  // ----------------------------------------------------
+  // Import Grunt module dependencies
+  // ----------------------------------------------------
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-shell');
+
+
+  // ----------------------------------------------------
+  // Register multi-tasks and helper methods
+  // ----------------------------------------------------
+};
