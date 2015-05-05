dd67ded55b0f45c2e22d7af3de7b9d2dbc3317de
vokoshyv <ronsuij@gmail.com>
2015-05-05 16:02:50 -0700

    (test) Test personal repo to personal repo rebasing

--- server/users/userController.js
+++ server/users/userController.js
@@ -2,8 +2,10 @@
 * @Author: vokoshyv
 * @Date:   2015-05-05 09:56:42
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-05-05 10:19:39
+* @Last Modified time: 2015-05-05 16:01:51
 */
 
 
 //userController.js
+//
+//THIS IS A TEST
