486ea3da56135c2a1cc25b22b8e9a7669032ceb8
VINCE <vtam90@gmail.com>
2015-05-05 16:02:13 -0700

    (feat) Create login module

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -2,11 +2,29 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-05 18:11:44
+* @Last Modified time: 2015-05-05 20:00:53
 */
 
 'use strict';
 (function (angular, _) {
+
+  var LoginConfig = function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: '/login/login.tpl.html',
+        controller: LoginCtrl
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: '/login/login.tpl.html',
+        controller: LoginCtrl
+      });
+
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise('/login');
+  };
 
   /**
    * Sets viewstate for login page
