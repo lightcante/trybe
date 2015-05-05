39874d7bc75ad4532da7084b9aaf4c878b434fe2
vokoshyv <ronsuij@gmail.com>
2015-05-05 15:39:30 -0700

    (fix) Fix DataTypes typo

--- server/models/trybeSchema.js
+++ server/models/trybeSchema.js
@@ -1,14 +1,14 @@
 /* 
 * @Author: nimi
 * @Date:   2015-05-05 13:31:44
-* @Last Modified by:   nimi
-* @Last Modified time: 2015-05-05 14:08:37
+* @Last Modified by:   vokoshyv
+* @Last Modified time: 2015-05-05 15:25:05
 */
 
 'use strict';
 
 module.exports = function (sequelize, DataTypes) {
   return sequelize.define('Trybes', {
-    name: sequelize.STRING
+    name: DataTypes.STRING
   })
 }
