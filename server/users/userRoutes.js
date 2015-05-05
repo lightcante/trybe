2863cc51790b3b2372aa79fadb8b620f819da99e
vokoshyv <ronsuij@gmail.com>
2015-05-05 11:41:25 -0700

    (cleanup) Add 'use strict' to top of userRoutes.js file

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,18 +2,16 @@
 * @Author: vokoshyv
 * @Date:   2015-05-05 09:56:42
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-05-05 11:26:05
+* @Last Modified time: 2015-05-05 11:39:19
 */
-
-
-//userRoutes.js
-
+'use strict';
 var userController = require('./userController.js');
 
 module.exports = function(app){
-  // this app was injected from the middleware line 33;
+  // this app was injected from the middleware line 33
 
   app.post('/signin', userController.signin);
   app.post('/signup', userController.signup);
   app.get('/signedin', userController.checkAuth);
-}
+
+};
