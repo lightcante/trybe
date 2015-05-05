1e2a15624cef6d49fa9cf133c03066d732c9e4d5
vokoshyv <ronsuij@gmail.com>
2015-05-05 16:05:34 -0700

    (test) Rebase from nimi's repo to see own commits added

--- server/users/userController.js
+++ server/users/userController.js
@@ -2,10 +2,12 @@
 * @Author: vokoshyv
 * @Date:   2015-05-05 09:56:42
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-05-05 16:01:51
+* @Last Modified time: 2015-05-05 16:04:59
 */
 
 
 //userController.js
 //
 //THIS IS A TEST
+//
+//Ron's addition
