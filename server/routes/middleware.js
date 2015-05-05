102c27b3f3624360ab120867a502063277c9f3a0
vokoshyv <ronsuij@gmail.com>
2015-05-05 12:00:46 -0700

    (feat) Add morgan dependency to middleware.js

--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -2,15 +2,13 @@
 * @Author: vokoshyv
 * @Date:   2015-05-05 10:14:44
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-05-05 11:05:50
+* @Last Modified time: 2015-05-05 12:00:16
 */
-
-
-
+'use strict';
 //this file will be used to interpret and route http requests
 
-
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 module.exports = function(app, express){
 
@@ -19,8 +17,18 @@ module.exports = function(app, express){
   var userRouter = express.Router();
   var workoutRouter = express.Router();
 
-  app.use(boydParser.urlencoded({extended: true}));
+  //Morgan allows us to automatically log requests and
+  //responses
+  app.use(morgan('dev'));
+
+  //bodyParser takes in the http requests and attaches
+  //the data object to the request.body ("POST" requests)
+  //or request.header ("GET" requests);
+  app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+
+  //express.static allows us to look up files in the client
+  //folder
   app.use(express.static(__dirname + '/../../client'));
 
   // use userRouter for user requests
@@ -33,4 +41,4 @@ module.exports = function(app, express){
   require('../users/userRoutes.js')(userRouter);
   require('../workout/workoutRoutes.js')(workoutRouter);
 
-}
+};
