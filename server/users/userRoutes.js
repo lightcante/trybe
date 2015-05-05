833b4c551d266239b29c8f05f191506bf4c7e578
vokoshyv <ronsuij@gmail.com>
2015-05-05 11:26:23 -0700

    (feat) Add user routes for /api/users/.. requests

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,8 +2,18 @@
 * @Author: vokoshyv
 * @Date:   2015-05-05 09:56:42
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-05-05 10:19:31
+* @Last Modified time: 2015-05-05 11:26:05
 */
 
 
 //userRoutes.js
+
+var userController = require('./userController.js');
+
+module.exports = function(app){
+  // this app was injected from the middleware line 33;
+
+  app.post('/signin', userController.signin);
+  app.post('/signup', userController.signup);
+  app.get('/signedin', userController.checkAuth);
+}
