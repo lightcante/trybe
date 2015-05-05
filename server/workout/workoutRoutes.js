89fe73ae54ecbb05e1f3e348c5aa9478144c5392
vokoshyv <ronsuij@gmail.com>
2015-05-05 11:40:18 -0700

    (feat) Add workout routes for /api/workouts requests

--- server/workout/workoutRoutes.js
+++ server/workout/workoutRoutes.js
@@ -1,1 +1,16 @@
-//workoutRoutes.js
+/* 
+* @Author: vokoshyv
+* @Date:   2015-05-05 09:56:42
+* @Last Modified by:   vokoshyv
+* @Last Modified time: 2015-05-05 11:39:10
+*/
+'use strict';
+var workoutController = require('./workoutController.js');
+
+module.exports = function(app){
+  // this app was injected from the middleware line 34
+  
+  app.post('/', workoutController.saveWorkout);
+  app.get('/', workoutController.getAllWorkouts);
+
+};
