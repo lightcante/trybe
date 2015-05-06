daeafe985a2fc4eb5162a4cc099a8830f439322e
vokoshyv <ronsuij@gmail.com>
2015-05-06 10:43:33 -0700

    (feat) Create dummy data return for workout controller

--- server/workout/workoutController.js
+++ server/workout/workoutController.js
@@ -1,7 +1,19 @@
 /* 
 * @Author: nimi
 * @Date:   2015-05-04 16:41:47
-* @Last Modified by:   nimi
-* @Last Modified time: 2015-05-05 16:05:28
+* @Last Modified by:   vokoshyv
+* @Last Modified time: 2015-05-06 10:29:50
 */
+'use strict';
 
+module.exports = {
+
+  saveWorkout: function(req, res, next){
+    res.send({"IT WORKS" : "IT WORKS"});
+  },
+
+  getAllWorkouts: function(req, res, next){
+    res.send({"THIS ONE TOO": "THIS ONE TOO"});
+  }
+
+};
--- server/workout/workoutRoutes.js
+++ server/workout/workoutRoutes.js
@@ -1,16 +1,16 @@
 /* 
 * @Author: vokoshyv
 * @Date:   2015-05-05 09:56:42
-* @Last Modified by:   nimi
-* @Last Modified time: 2015-05-05 17:33:24
+* @Last Modified by:   vokoshyv
+* @Last Modified time: 2015-05-06 10:32:01
 */
 'use strict';
 var workoutController = require('./workoutController.js');
 
 module.exports = function(app){
   // this app was injected from the middleware line 34
-  
-  // app.post('/', workoutController.saveWorkout);
-  // app.get('/', workoutController.getAllWorkouts);
+  console.log('here');
+  app.post('/', workoutController.saveWorkout);
+  app.get('/', workoutController.getAllWorkouts);
 
 };
