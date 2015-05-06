7a1d97c269986cada76bead746205185d55b022f
VINCE <vtam90@gmail.com>
2015-05-05 17:03:30 -0700

    (feat) Update login view to save username and password

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -2,43 +2,48 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-05 15:58:39
+* @Last Modified time: 2015-05-05 16:59:58
 */
 
 'use strict';
 (function (angular, _) {
 
-  var LoginConfig = function($stateProvider, $urlRouterProvider) {
+  /**
+   * Sets viewstate for login page
+   * @param {[angular]} $stateProvider
+   */
+  var LoginConfig = function($stateProvider) {
 
     $stateProvider
       .state('login', {
         url: '/login',
-        templateUrl: '/login/login.tpl.html',
+        templateUrl: 'client/app/login/login.tpl.html',
         controller: LoginCtrl
       })
       .state('signup', {
         url: '/signup',
-        templateUrl: '/login/login.tpl.html',
+        templateUrl: 'client/app/login/login.tpl.html',
         controller: LoginCtrl
       });
-
-    // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise('/login');
   };
 
   /**
-   * controls application access from the client side. Has
+   * Controls login and signup for client side. Has
    * states for login and signup.
    * @param {angular} $scope
    */
   var LoginCtrl = function ($scope) {
-
+    $scope.isUser = true;
+    $scope.username;
+    $scope.password;
   };
 
   // Entry point for module
   angular
 
-    .module('app', [])
+    .module('trybe-app.login', [])
+
+    .config(LoginConfig)
 
     .controller('LoginCtrl', LoginCtrl);
 
