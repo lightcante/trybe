8861e486240baecd99fc72a46744637df7d2696e
VINCE <vtam90@gmail.com>
2015-05-06 16:46:05 -0700

    (refactor) Refactor login module for clarity

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-10 22:32:26
+* @Last Modified time: 2015-05-10 22:34:20
 */
 
 'use strict';
