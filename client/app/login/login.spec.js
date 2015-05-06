91985e7cf48193e15dabea06f5e7baec6f61b833
VINCE <vtam90@gmail.com>
2015-05-06 13:22:22 -0700

    (test) Set up initial unit test for login module

--- client/app/login/login.spec.js
+++ client/app/login/login.spec.js
@@ -1,8 +1,40 @@
-/* 
+/*
 * @Author: justinwebb
 * @Date:   2015-05-04 16:04:11
-* @Last Modified by:   justinwebb
-* @Last Modified time: 2015-05-04 16:04:19
+* @Last Modified by:   vincetam
+* @Last Modified time: 2015-05-06 12:23:29
 */
 
-'use strict';
+console.log("***Login spec***");
+
+describe('Login Controller', function() {
+  beforeEach(module('trybe-app.login'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.isUser', function() {
+    it('should be true', function() {
+      var $scope = {};
+      var controller = $controller('LoginCtrl', { $scope: $scope });
+      expect($scope.isUser).toEqual(true);
+      var test = true;
+      expect(test).toEqual(true);
+    });
+  });
+
+  // describe('$scope.grade', function() {
+  //   it('sets the strength to "strong" if the password length is >8 chars', function() {
+  //     var $scope = {};
+  //     var controller = $controller('PasswordController', { $scope: $scope });
+  //     $scope.password = 'longerthaneightchars';
+  //     $scope.grade();
+  //     expect($scope.strength).toEqual('strong');
+  //   });
+  // });
+
+});
