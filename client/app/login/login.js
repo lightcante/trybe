d34328fbfdb161d08d138ce4640eca571ae853ab
VINCE <vtam90@gmail.com>
2015-05-06 18:17:54 -0700

    (fix) Fix bug in feed and refactor login

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-08 00:18:35
+* @Last Modified time: 2015-05-08 00:19:17
 */
 
 'use strict';
