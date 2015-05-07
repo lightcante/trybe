575416706cdeae8a67a7793130a8b97d2b54ce31
VINCE <vtam90@gmail.com>
2015-05-06 18:14:40 -0700

    (feat) Set up feed factory

---
+++ client/app/common/services/feed.factory.js
@@ -0,0 +1,41 @@
+/*
+* @Author: vincetam
+* @Date:   2015-05-06 18:01:45
+* @Last Modified by:   vincetam
+* @Last Modified time: 2015-05-06 18:10:44
+*/
+
+'use strict';
+(function (angular, _) {
+
+  /**
+   * Handles auth services for login view
+   * @param {[angular]} $http
+   * @param {[angular]} $location
+   * @param {[angular]} $window
+   */
+  var FeedFactory = function ($http, $location, $window, FeedFactory) {
+    var getWorkouts = function () {
+      return $http({
+        method: 'GET',
+        url: '/api/workouts',
+        data: {'ORDERING_CRITERIA_KEY': 'ORDERING_CRITERIA_VALUE'} //optional
+      })
+      .then(function (resp) {
+        return resp.data; //sends back data to controller
+      });
+    };
+
+    return {
+      getWorkouts: getWorkouts
+    };
+  };
+
+angular
+
+  .module('trybe-app.common', ['trybe-app.common'])
+
+  .factory('FeedFactory', FeedFactory);
+
+})(angular, _);
+
