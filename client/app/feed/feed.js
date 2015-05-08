461775a5e330096de01e3c1f00f5d9112016b14e
VINCE <vtam90@gmail.com>
2015-05-08 09:32:02 -0700

    (fix) Edit code to conform to style guide

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-11 10:57:27
+* @Last Modified time: 2015-05-11 10:58:51
 */
 
 'use strict';
