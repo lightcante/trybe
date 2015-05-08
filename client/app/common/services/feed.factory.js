780539615af859a741d3ffa68de157bb82e42935
VINCE <vtam90@gmail.com>
2015-05-08 11:00:12 -0700

    (fix) Fix bug in profile and feed factory where each referenced itself

--- client/app/common/services/feed.factory.js
+++ client/app/common/services/feed.factory.js
@@ -2,7 +2,7 @@
 * @Author: vincetam
 * @Date:   2015-05-06 18:01:45
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-11 10:57:40
+* @Last Modified time: 2015-05-11 11:02:27
 */
 
 'use strict';
