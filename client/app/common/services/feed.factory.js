907c9d119ea6ba8b910cea12485a1d5b21e2f1ce
VINCE <vtam90@gmail.com>
2015-05-08 11:16:30 -0700

    (feat) Set up initial structure for profile state

--- client/app/common/services/feed.factory.js
+++ client/app/common/services/feed.factory.js
@@ -1,8 +1,8 @@
 /*
 * @Author: vincetam
 * @Date:   2015-05-06 18:01:45
-* @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-07 23:25:20
+* @Last Modified by:   VINCE
+* @Last Modified time: 2015-05-08 11:07:35
 */
 
 'use strict';
@@ -36,7 +36,7 @@
     var getWorkout = function() {
       console.log('workout req from log');
       return workout;
-    }
+    };
 
     return {
       getWorkouts: getWorkouts,
@@ -47,7 +47,7 @@
 
 angular
 
-  .module('trybe-app.common', ['trybe-app.common'])
+  .module('trybe-app.common')
 
   .factory('FeedFactory', FeedFactory);
 
