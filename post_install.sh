6de3bd89068b8b555b95187afe35519fc8657169
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-05-11 14:58:12 -0700

    (fix) Install client dependencies before running grunt

--- post_install.sh
+++ post_install.sh
@@ -2,10 +2,11 @@
 # @Author: nimi
 # @Date:   2015-05-11 13:02:29
 # @Last Modified by:   nimi
-# @Last Modified time: 2015-05-11 13:06:06
+# @Last Modified time: 2015-05-11 14:45:22
 
-#run grunt- default will build the app
-./node_modules/grunt-cli/bin/grunt
 
 #install client side dependencies
 ./node_modules/bower/bin/bower install
+
+#run grunt- default will build the app
+./node_modules/grunt-cli/bin/grunt
