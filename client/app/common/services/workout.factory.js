b3701af714dc0caf1f108e2bb998319ddb178610
VINCE <vtam90@gmail.com>
2015-05-27 21:54:45 -0700

    (refactor) Refactor workout module for clarity

--- client/app/common/services/workout.factory.js
+++ client/app/common/services/workout.factory.js
@@ -2,7 +2,7 @@
 * @Author: vincetam
 * @Date:   2015-05-06 18:01:45
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-27 21:25:20
+* @Last Modified time: 2015-05-27 21:40:10
 */
 
 'use strict';
@@ -16,7 +16,6 @@
    */
   var WorkoutFactory = function ($http, $location, $window) {
     var workout;
-    var allWorkouts;
     var isNewWorkout;
     var workoutSelectionStore = 'com.trybe.selectedWorkout';
     var localStorage = $window.localStorage;
